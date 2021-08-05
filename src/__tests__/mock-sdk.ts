2173cef3719d3006b7fc147d42cfffe857ded097
Yesha  Mavani <yesha.mavani@sourcefuse.com>
2021-08-05 17:30:18 +0530

    notification testcase and coverage

---
+++ src/__tests__/mock-sdk.ts
@@ -0,0 +1,40 @@
+import AWS from 'aws-sdk';
+import Pubnub from 'pubnub';
+import Mail = require('nodemailer/lib/mailer');
+import SMTPTransport = require('nodemailer/lib/smtp-transport');
+
+export class MockSES {
+  constructor(config: AWS.SES.Types.ClientConfiguration) {}
+
+  async sendEmail(emailReq: AWS.SES.SendEmailRequest) {}
+}
+
+export class MockSNS {
+  constructor(config: AWS.SNS.ClientConfiguration) {}
+
+  async publish(message: AWS.SNS.PublishInput) {}
+}
+
+export class MockSocketIo {
+  constructor(
+    url: string,
+    options?: {
+      [key: string]: string;
+    },
+  ) {}
+
+  async emit(path: string, message: string) {}
+}
+
+export class MockMail {
+  constructor(config: SMTPTransport.Options) {}
+
+  async sendMail(message: Mail.Options) {}
+}
+
+export class MockPubnub {
+  constructor(config: Pubnub.PubnubConfig) {}
+
+  grant(grantConfig: Pubnub.GrantParameters) {}
+  async publish(publishConfig: Pubnub.PublishParameters) {}
+}
