c349d904ce8347ed9eb242ef9eda95ef0098a7d3
Samarpan  Bhattacharya <samarpan.bhattacharya@Samarpans-MacBook-Air.local>
2023-01-15 19:33:49 +0530

    fix(provider): make apns provider optional
    @parse/node-apn was a dependency but its an optional feature. So its moved as a dev dependency and
    exported as optional providers.
    
    gh-0

--- src/component.ts
+++ src/component.ts
@@ -2,12 +2,16 @@ import {Binding, Component, ProviderMap} from '@loopback/core';
 import {NotificationBindings} from './keys';
 import {SESBindings} from './providers/email/ses/keys';
 import {NotificationProvider} from './providers/notification.provider';
+import {ApnsBinding} from './providers/push/apns/keys';
+import {FcmBindings} from './providers/push/fcm/keys';
 import {PubnubBindings} from './providers/push/pubnub/keys';
 import {SocketBindings} from './providers/push/socketio/keys';
 import {SNSBindings} from './providers/sms/sns/keys';
 
 export class NotificationsComponent implements Component {
-  constructor() {}
+  constructor() {
+    // Intentionally left blank
+  }
 
   providers?: ProviderMap = {
     [NotificationBindings.NotificationProvider.key]: NotificationProvider,
@@ -19,5 +23,7 @@ export class NotificationsComponent implements Component {
     Binding.bind(SNSBindings.Config.key).to(null),
     Binding.bind(PubnubBindings.Config.key).to(null),
     Binding.bind(SocketBindings.Config.key).to(null),
+    Binding.bind(ApnsBinding.Config.key).to(null),
+    Binding.bind(FcmBindings.Config.key).to(null),
   ];
 }
